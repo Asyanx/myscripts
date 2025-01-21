13a2745c64ab497c48dfed430a71716e4147b49b
Alixxxxxxxixixi <76146196+Asyanx@users.noreply.github.com>
2025-01-22 00:04:13 +0700

    + verssion

--- kernel.sh
+++ kernel.sh
@@ -24,7 +24,7 @@ KERNEL="$WORKDIR/kernel"
 
 # Cloning Sources
 git clone --single-branch --depth=1 https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 -b kila-r-oss $KERNEL && cd $KERNEL
-export LOCALVERSION=✨🫀
+export LOCALVERSION=+✨🫀
 
 # Bail out if script fails
 set -e
