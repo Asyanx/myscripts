74e8adab849c17c0081e8201c7ea8088d3bd6b44
Alixxxxxxxixixi <76146196+Asyanx@users.noreply.github.com>
2025-02-28 13:23:18 +0700

    SPESIAL DONATUR

--- kernel.sh
+++ kernel.sh
@@ -24,7 +24,7 @@ KERNEL="$WORKDIR/kernel"
 
 # Cloning Sources
 git clone --single-branch --depth=1 https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 -b kila-r-oss $KERNEL && cd $KERNEL
-export LOCALVERSION=+✨🦩
+export LOCALVERSION=🧏🏻‍♀️🎀
 
 # Bail out if script fails
 set -e
@@ -54,8 +54,8 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # PATCH KERNELSU & RELEASE VERSION
-KSU=0
-RELEASE=R5+
+KSU=1
+RELEASE=XR1+
 if [ $KSU = 1 ]
 then
 #	echo "CONFIG_KSU=y" >> arch/arm64/configs/vendor/"fog-perf_defconfig"
@@ -66,7 +66,7 @@ then
 fi
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="Sea"
+ZIPNAME="poppyyy"
 if [ $KSU = 1 ]
 then
    VER="$RELEASE-KSU"
@@ -76,8 +76,8 @@ fi
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
-AUTHOR="AliStill"
-HOSTR="LoveUU"
+AUTHOR="poppyyyStill"
+HOSTR="LoveuU"
 
 # Architecture
 ARCH=arm64
