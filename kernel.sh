673878ed405b401a17fbcb79ad2bebc598c8cde0
Alixxxxxxxixixi <76146196+Asyanx@users.noreply.github.com>
2026-08-21 20:30:04 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -23,7 +23,7 @@ WORKDIR="$(pwd)"
 KERNEL="$WORKDIR/kernel"
 
 # Cloning Sources
-git clone --single-branch --depth=1 https://github.com/Asyanx/kernel-whyred-4.19 -b android-4.19-stable $KERNEL && cd $KERNEL
+git clone --single-branch --depth=1 https://github.com/Asyanx/kernel-whyred-4.19 -b back $KERNEL && cd $KERNEL
 export LOCALVERSION=+🐝
 
 # Bail out if script fails
@@ -61,7 +61,7 @@ RELEASE=R1
 ZIPNAME="Sea"
 if [ $KSU = 1 ]
 then
-   VER="$RELEASE-KSU-419"
+   VER="$RELEASE-KSU-419-16QPR2"
 else
     VER="$RELEASE"
 fi
